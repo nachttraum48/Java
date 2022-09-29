534611d8436b67a63e288cdc5864505aa29dc8c8
nachttraum48 <nachttraum48@gmail.com>
2022-09-29 19:52:32 +0900

    2022/09/29 수업예제

---
+++ Exam/src/sub08/Test09.java
@@ -0,0 +1,41 @@
+package sub08;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/*
+ * 날짜 : 2022/09/29
+ * 이름 : 이석중
+ * 내용 : 자바 총정리 연습문제
+ * 
+ */
+public class Test09 {
+
+	public static void main(String[] args) {
+		
+		String path = "C:\\Users\\java2\\Desktop\\Gugudan.txt";
+		
+		try {
+			File file = new File(path);			
+			FileWriter fw = new FileWriter(file);			
+			
+			for(int x=2 ; x<=9 ; x++) {
+				
+				fw.write(x + "단\n");
+				
+				for(int y=1 ; y<=9 ; y++) {
+					int z = x * y;					
+					fw.write(x + " x " + y + " = " + z + "\n");			
+				}
+			}			
+			
+			fw.close();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println("구구단 파일생성 완료...");
+	}	
+}
